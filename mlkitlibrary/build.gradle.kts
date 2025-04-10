21716598f206496ae4f08b9a9c66f17007ecd251
wulaizi <742241699@qq.com>
2025-04-10 10:09:24 +0800

    给该库降级适配gradle 8.0.1

--- mlkitlibrary/build.gradle.kts
+++ mlkitlibrary/build.gradle.kts
@@ -1,15 +1,17 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.maven.publish)
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
     namespace = "com.extra.mlkitlibrary"
     compileSdk = 34
+    buildToolsVersion = "34.0.0"
 
     defaultConfig {
-        minSdk = 24
+        minSdk = 23
+        targetSdk = 34
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -31,27 +33,43 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         buildConfig = true
+        aidl = true
+        renderScript = true
+        resValues = true
+        shaders = true
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
+    ndkVersion = "26.1.10909125"
 }
 
 dependencies {
-    implementation(libs.mlkit.chinese)
-    implementation(libs.mlkit.all)
-    implementation(libs.bouncycastle)
-    implementation(libs.okhttp)
-    implementation(libs.net)
-    implementation(libs.blankj.utilcodex)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.google.mlkit:text-recognition-chinese:16.0.0")
+    implementation("com.google.mlkit:text-recognition:16.0.0")
+    implementation("org.bouncycastle:bcprov-jdk15on:1.70")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.blankj:utilcodex:1.31.1")
+    
+    // Net 网络请求库
+    implementation("com.github.liangjingkanji:Net:3.5.5")
+    
+    // Kotlin Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    
+    // AndroidX Core
+    implementation("androidx.core:core-ktx:1.12.0")
+    
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
 afterEvaluate{
@@ -68,15 +86,16 @@ afterEvaluate{
             }
         }
 
-//        repositories {
-//            maven {
-//                setUrl("https://packages.aliyun.com/60e5087c2f222be2d940156b/maven/2119021-release-oxlyuf")
-//                credentials {
-//                    username = "611cb37ccd146a5e9cc23b6e"
-//                    password = "J7IZlu4Xge3S"
-//                }
-//            }
-//        }
+        // repositories {
+        //     maven {
+        //         name = "GitPack"
+        //         url = uri("https://gitpack.io/com/github/extracod/mlkitmanager")
+        //         credentials {
+        //             username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+        //             password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+        //         }
+        //     }
+        // }
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,44 +1,19 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
-        maven(url = "https://jitpack.io")
-        maven(url = "https://maven.aliyun.com/repository/public")
-        maven {
-            credentials {
-                username = "611cb37ccd146a5e9cc23b6e"
-                password = "J7IZlu4Xge3S"
-            }
-            setUrl("https://packages.aliyun.com/60e5087c2f222be2d940156b/maven/2119021-release-oxlyuf")
-        }
         gradlePluginPortal()
     }
 }
 
-@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven(url = "https://jitpack.io")
-        maven(url = "https://maven.aliyun.com/repository/public")
-        maven {
-            credentials {
-                username = "611cb37ccd146a5e9cc23b6e"
-                password = "J7IZlu4Xge3S"
-            }
-            setUrl("https://packages.aliyun.com/60e5087c2f222be2d940156b/maven/2119021-release-oxlyuf")
-        }
+        maven { url = uri("https://jitpack.io") }
     }
 }
-
 
 rootProject.name = "MlKitLib"
 include(":app")
